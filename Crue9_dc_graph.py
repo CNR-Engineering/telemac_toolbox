954972faf78f17eb45ec8ab0eae271c240cf1f2a
lucduron <duron.luke@gmail.com>
2017-12-13 14:46:34 +0100

    Ajout des schémas topologiques

---
+++ Crue9_dc_graph.py
@@ -0,0 +1,167 @@
+#!/usr/bin/env python
+"""
+@brief:
+Générer un schéma topologique sous forme d'image png à partir d'un fichier dc (fichier de géométrie de Crue 9)
+
+@info:
+* il s'agit d'une vue schématique avec tous les noeuds et les branches du modèle (aucune information géographique)
+* toutes les branches sont orientées de l'amont vers aval (mais certains types de branches ne devraient pas être orientées)
+* la coloration dépent du type de branches et les casiers et noeuds ont des formes différentes
+* les parties commentées ou shunter (par un GOTO) sont ignorées
+* Les noms des branches et noeuds sont écrits en masjuscules
+* les mots-clés (BRANCHE et GOTO) peuvent être indifférement en minuscules ou en majuscules.
+* l'espacement est réglable avec l'option `--sep`
+
+@warnings:
+L'image de sortie est écrasée si elle existe
+"""
+# Tout le fichier dc est lu et les variables affectées
+# Ensuite l'arbre puis le graphique sont générés
+
+import argparse
+import sys
+
+
+# Branches
+TYPE_BRANCHES = {
+    'BrancheBarrageFilEau': '15',
+    # 'BrancheBarrageGenerique': '?',
+    'BrancheSeuilTransversal': '2',
+    'BrancheStrickler': '6',
+    'BrancheSeuilLateral': '4',
+    'BrancheSaintVenant': '20',
+    'BranchePdc': '1',
+    'BrancheOrifice': '5',
+    # 'BrancheNiveauxAssocies': '?',
+    }
+
+ARROWHEAD = {
+    '2': 'curve',
+    '4': 'tee',
+    '5': 'odot',
+    '6': 'diamond',
+    '15': 'box',
+    'default': 'normal'
+    # 'BrancheNiveauxAssocies': '?',
+}
+
+# Couleurs selon le type de branche
+COLORS = {
+    '4': 'darkgreen',
+    '5': 'green',
+    '6': 'purple',
+    '9': 'navy',
+    '20':'blue',
+    'default': 'black'
+}
+
+# Taille de la branche
+SIZE = {
+    '9': 4,
+    '15': 4,
+    '20': 4,
+    'default': 2
+}
+
+def key_from_constant(key, CONSTANT):
+    """Retourne la valeur de key du dictionnanire CONSTANT"""
+    try:
+        return CONSTANT[key]
+    except KeyError:
+        try:
+            return CONSTANT['default']
+        except KeyError:
+            sys.exit("La clé '{}' n'existe pas".format(key))
+
+parser = argparse.ArgumentParser(
+    formatter_class=argparse.RawDescriptionHelpFormatter,
+    description=(__doc__))
+parser.add_argument("fichier_dc", help="fichier d'entrée dc (format géométrie Crue9)")
+parser.add_argument("--out_png", help="fichier de sortie au format png")
+parser.add_argument("--out_svg", help="fichier de sortie au format svg")
+parser.add_argument("--sep", help="ratio pour modifier l'espacement (par ex. 0.5 ou 2) [1 par défaut]", default=1)
+args = parser.parse_args()
+
+dc_file = args.fichier_dc
+with open(dc_file, 'r', encoding="ISO-8859-1") as filein:
+    print("Traitement du fichier {}".format(dc_file))
+    branches = {}  # dictionnaire de la forme {nom_branche: (noeud_amont, noeud_aval, type)}
+    nodes = []  # liste de noeuds
+    casiers = []  # liste de noeuds
+    label = None  # pour les goto
+
+    for line in filein:
+        line = line.replace('\n', '')
+        line = line.upper()  # nom des branches/noeuds non-sensible à la casse
+
+        if not line.startswith('*'):
+            if label is not None:
+                # Il y a un goto en cours...
+                if line.startswith(label):
+                    print("/!\ Partie shunter par un GOTO de label : {}".format(label))
+                    label = None
+
+            else:
+                # On est en dehors du goto
+                if line.startswith('GOTO'):
+                    # Mais on rentre dans un autre GOTO...
+                    (key, label) = line.split()
+
+                elif line.startswith('BRANCHE'):
+                    # Une nouvelle branche est trouvée
+                    (key, name, node_up, node_down, btype) = line.split()
+                    print("Ajout de la branche {} ({} -> {})".format(name, node_up, node_down))
+
+                    # Ajout des noeuds si non présents
+                    if node_up not in nodes:
+                        nodes.append(node_up)
+                    if node_down not in nodes:
+                        nodes.append(node_down)
+
+                    # Ajout de la branche
+                    branches[name] = (node_up, node_down, btype)
+
+                elif line.startswith('CASIER'):
+                    (key, node) = line.split()
+                    casiers.append(node)
+                    print("Casier {} détecté".format(node))
+
+try:
+    import pydot
+except:
+    sys.exit("Le module pydot ne fonctionne pas !")
+
+# Création de l'arbre
+graph = pydot.Dot(graph_type='digraph', nodesep=args.sep)  # vertical : rankdir='LR'
+
+## Ajout des noeuds
+for node in nodes:
+    if node in casiers:
+        shape = 'box3d'
+    else:
+        shape = 'ellipse'
+    graph.add_node(pydot.Node(node, style="filled", fillcolor="white", shape=shape))
+
+## Ajout des branches
+for nom_branche, (node_up, node_down, btype) in branches.items():
+    edge = pydot.Edge(node_up, node_down,
+                      arrowhead=key_from_constant(btype, ARROWHEAD),
+                      # arrowtail="inv",
+                      label=nom_branche,
+                      color=key_from_constant(btype, COLORS),
+                      fontcolor=key_from_constant(btype, COLORS),
+                      penwidth=key_from_constant(btype, SIZE)
+                      # arrowtail="normal",
+                      # dirType="back", marche pas
+                      # shape="dot"
+    )
+    graph.add_edge(edge)
+
+# Export(s) en png et/ou svg
+# prog='neato' optimise l'espace
+if args.out_png:
+    print("Génération du fichier {}".format(args.out_png))
+    graph.write_png(args.out_png)
+if args.out_svg:
+    print("Génération du fichier {}".format(args.out_svg))
+    graph.write_svg(args.out_svg)
---
+++ FC_graphe-topologique.py
@@ -0,0 +1,174 @@
+#!/usr/bin/env python
+"""
+@brief:
+Générer un schéma topologique sous forme d'image png/svg à partir d'une étude FC (fichier drso)
+
+@warnings:
+Les fichiers de sortie est écrasée si elle existe
+
+TODO:
+* les orifices ne sont pas orientés en réalité (Pb: avec le type de graph 'digraph' (DIrected graph) cela ne semble pas possible d'avoir les deux sur la double flèche)
+* fusionner avec le script Crue_dc_graph.py + faire du Qt
+"""
+
+import argparse
+import sys
+import xml.etree.ElementTree as ET
+
+
+# Branches
+TYPE_BRANCHES = {
+    'BrancheBarrageFilEau': '15',
+    # 'BrancheBarrageGenerique': '?',
+    'BrancheSeuilTransversal': '2',
+    'BrancheStrickler': '6',
+    'BrancheSeuilLateral': '4',
+    'BrancheSaintVenant': '20',
+    'BranchePdc': '1',
+    'BrancheOrifice': '5',
+    # 'BrancheNiveauxAssocies': '?',
+    }
+
+ARROWHEAD = {
+    '2': 'curve',
+    '4': 'tee',
+    '5': 'odot',
+    '6': 'diamond',
+    '15': 'box',
+    'default': 'normal'
+    # 'BrancheNiveauxAssocies': '?',
+}
+
+# Couleurs selon le type de branche
+COLORS = {
+    '4': 'darkgreen',
+    '5': 'green',
+    '6': 'purple',
+    '9': 'navy',
+    '20':'blue',
+    'default': 'black'
+}
+
+# Taille de la branche
+SIZE = {
+    '9': 4,
+    '15': 4,
+    '20': 4,
+    'default': 2
+}
+
+def key_from_constant(key, CONSTANT):
+    """Retourne la valeur de key du dictionnanire CONSTANT"""
+    try:
+        return CONSTANT[key]
+    except KeyError:
+        try:
+            return CONSTANT['default']
+        except KeyError:
+            sys.exit("La clé '{}' n'existe pas".format(key))
+
+def remove_prefix(text, prefix):
+    if text.startswith(prefix):
+        return text[len(prefix):]
+    else:
+        return text
+
+
+PREFIX = "{http://www.fudaa.fr/xsd/crue}"
+ET.register_namespace('', "http://www.fudaa.fr/xsd/crue")
+
+
+parser = argparse.ArgumentParser(
+    formatter_class=argparse.RawDescriptionHelpFormatter,
+    description=(__doc__))
+parser.add_argument("fichier_drso", help="fichier d'entrée drso")
+parser.add_argument("--out_png", help="fichier de sortie au format png")
+parser.add_argument("--out_svg", help="fichier de sortie au format svg")
+parser.add_argument("--sep", help="ratio pour modifier l'espacement (par ex. 0.5 ou 2) [1 par défaut]", default=1)
+parser.add_argument("--remove_preffix", help="suppression des préfixes des emh ('Br_' et 'St_')", action='store_true')
+args = parser.parse_args()
+
+
+branches = {}  # dictionnaire de la forme {nom_branche: (noeud_amont, noeud_aval, type)}
+nodes = []  # liste de noeuds
+casiers = []  # liste de noeuds
+
+# drso
+for emh_group in ET.parse(args.fichier_drso).getroot():
+    if emh_group.tag == (PREFIX+'Branches'):
+        for branche in emh_group:
+            is_active = branche.find(PREFIX+'IsActive').text  # a string : "true" or "false"
+
+            if is_active=="true":
+                type_branche = branche.tag.replace(PREFIX, '')
+                name = branche.attrib['Nom']
+                node_up = branche.find(PREFIX+'NdAm').attrib['NomRef']
+                node_down = branche.find(PREFIX+'NdAv').attrib['NomRef']
+                if args.remove_preffix:
+                    name = remove_prefix(name, 'Br_')
+                    node_up = remove_prefix(node_up, 'Nd_')
+                    node_down = remove_prefix(node_down, 'Nd_')
+                print("Ajout de la branche {} ({} -> {})".format(name, node_up, node_down))
+
+                btype = TYPE_BRANCHES[type_branche]
+
+                # Ajout des noeuds si non présents
+                if node_up not in nodes:
+                    nodes.append(node_up)
+                if node_down not in nodes:
+                    nodes.append(node_down)
+
+                # Ajout de la branche
+                branches[name] = (node_up, node_down, btype)
+
+    elif emh_group.tag == (PREFIX+'Casiers'):
+        for casier in emh_group:
+            is_active = casier.find(PREFIX+'IsActive').text  # a string : "true" or "false"
+
+            if is_active:
+                noeud = casier.find(PREFIX+'Noeud').attrib['NomRef']
+                print(noeud)
+                if args.remove_preffix:
+                    noeud = remove_prefix(noeud, 'Nd_')
+                casiers.append(noeud)
+
+try:
+    import pydot
+except:
+    sys.exit("Le module pydot ne fonctionne pas !")
+
+# Création de l'arbre
+graph = pydot.Dot(graph_type='digraph', nodesep=args.sep)  # vertical : rankdir='LR'
+
+## Ajout des noeuds
+for node in nodes:
+    if node in casiers:
+        shape = 'box3d'
+    else:
+        shape = 'ellipse'
+    graph.add_node(pydot.Node(node, style="filled", fillcolor="white", shape=shape))
+
+## Ajout des branches
+for nom_branche, (node_up, node_down, btype) in branches.items():
+    edge = pydot.Edge(node_up, node_down,
+                      arrowhead=key_from_constant(btype, ARROWHEAD),
+                      # arrowtail="inv",
+                      label=nom_branche,
+                      color=key_from_constant(btype, COLORS),
+                      fontcolor=key_from_constant(btype, COLORS),
+                      penwidth=key_from_constant(btype, SIZE)
+                      # arrowtail="normal",
+                      # dirType="back", marche pas
+                      # shape="dot"
+    )
+    graph.add_edge(edge)
+
+# Export(s) en png et/ou svg
+# prog='neato' optimise l'espace
+if args.out_png:
+    print("Génération du fichier {}".format(args.out_png))
+    graph.write_png(args.out_png)
+if args.out_svg:
+    print("Génération du fichier {}".format(args.out_svg))
+    graph.write_svg(args.out_svg)
+# graph.write('toto.dot')
