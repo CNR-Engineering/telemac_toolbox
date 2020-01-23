35d369222779b76e28102e04c186194febba14c4
Luc DURON <L.DURON@cnr.tm.fr>
2020-01-23 17:30:54 +0100

    shp_modify is used to copy, shift X/Y and/or order elements

--- shp_modify.py
+++ shp_modify.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
 """
 @brief:
-Order elements by a single field
-
-TODO:
-* mutliple fields
-* exception KeyError sur les fields
+Order elements (by a single field) and/or shift X,Y geometry
 """
-
 from collections import OrderedDict
 import fiona
+import shapely.affinity as aff
+from shapely.geometry import mapping, shape
 import sys
 
 from common.arg_command_line import myargparse
@@ -18,14 +15,14 @@ from common.arg_command_line import myargparse
 parser = myargparse(description=__doc__)
 parser.add_argument("inname", help="fichier d'entree shp")
 parser.add_argument("outname", help="fichier de sortie shp")
-parser.add_argument("field", help="attribut pour trier")
+parser.add_argument("--attr_to_order", help="attribut pour trier")
+parser.add_argument("--shift", type=float, nargs=2, help="décalage en X et Y (en mètre)")
 parser.add_argument("--reverse", help="inverser l'ordre de tri (par défaut: croissant)", action='store_true')
 args = parser.parse_args()
 
 
 elems = []
 with fiona.open(args.inname, 'r') as filein:
-    # with fiona.open(args.outname, 'w', 'ESRI Shapefile', schema) as layer:
     first_elem = filein[0]
     first_type = first_elem['geometry']['type']
     nb_elem = len(filein)
@@ -34,12 +31,15 @@ with fiona.open(args.inname, 'r') as filein:
     print("Nombres d'élements : {}".format(nb_elem))
 
     for i, elem in enumerate(filein):
-        print("{}: {}={}".format(i, args.field, elem['properties'][args.field]))
+        if args.attr_to_order:
+            print("{}: {}={}".format(i, args.attr_to_order, elem['properties'][args.attr_to_order]))
         if elem['geometry']['type'] != first_type:
             sys.exit("ERREUR: L'objet {} n'est pas de type {}".format(i, first_type))
         elems.append(elem)
 
-sorted_elems = sorted(elems, key=lambda x: x['properties'][args.field], reverse=args.reverse)
+if args.attr_to_order:
+    elems = sorted(elems, key=lambda x: x['properties'][args.attr_to_order], reverse=args.reverse)
+
 
 geom_type = first_type
 if len(first_elem['geometry']['coordinates'][0]):
@@ -51,9 +51,16 @@ for key, value in first_elem['properties'].items():
     properties[key] = type(value).__name__
 
 schema = {'geometry': geom_type, 'properties': properties}
-
-print("Ecriture des éléments triés")
+print("Ecriture des éléments triés avec ce schéma:")
+print(schema)
 with fiona.open(args.outname, 'w', 'ESRI Shapefile', schema) as layer:
-    for i, elem in enumerate(sorted_elems):
-        print("{}: {}={}".format(i, args.field, elem['properties'][args.field]))
+    for i, elem in enumerate(elems):
+
+        if args.shift is not None:
+            obj = shape(elem['geometry'])
+            obj = aff.translate(obj, xoff=args.shift[0], yoff=args.shift[1])
+            elem['geometry'] = mapping(obj)
+
+        if args.attr_to_order:
+            print("{}: {}={}".format(i, args.attr_to_order, elem['properties'][args.attr_to_order]))
         layer.write(elem)
